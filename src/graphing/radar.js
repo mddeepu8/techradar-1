67e751a944ea5bcdc572b51cc06371189a194ef2
Soney Mathew <smathew@atlassian.com>
2017-02-06 09:02:35 +1100

    footer updated plotRadarHeader h1 to h2

--- src/graphing/radar.js
+++ src/graphing/radar.js
@@ -374,7 +374,7 @@ const Radar = function (size, radar) {
       .attr('class', 'radar-title')
       .append('div')
       .attr('class', 'radar-title__text')
-      .append('h1')
+      .append('h2')
       .text(document.title)
       .style('cursor', 'pointer')
       .on('click', redrawFullRadar);
@@ -421,10 +421,7 @@ const Radar = function (size, radar) {
       .append('div')
       .attr('class', 'footer-content')
       .append('p')
-      .html('Powered by <a href="https://www.thoughtworks.com"> ThoughtWorks</a>. '
-      + 'By using this service you agree to <a href="https://info.thoughtworks.com/visualize-your-tech-strategy-terms-of-service.html">ThoughtWorks\' terms of use</a>. '
-      + 'You also agree to our <a href="https://www.thoughtworks.com/privacy-policy">privacy policy</a>, which describes how we will gather, use and protect any personal data contained in your public Google Sheet. '
-      + 'This software is <a href="https://github.com/thoughtworks/build-your-own-radar">open source</a> and available for download and self-hosting.');
+      .html('Based on <a href="https://github.com/thoughtworks/build-your-own-radar">ThoughtWorks Radar</a>.');
   }
 
   function mouseoverQuadrant(order) {
